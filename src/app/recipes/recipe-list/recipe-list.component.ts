3d96e32073697b7078fc4f8eb42db287796810cb
Simon Barker <simonbarker@MacBook-Pro-540.local>
2019-11-28 19:54:43 +0000

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe('Chilli Con Carne', 'A test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1200px-Good_Food_Display_-_NCI_Visuals_Online.jpg'),
+    // tslint:disable-next-line: max-line-length
+    new Recipe('Chicken curry', 'A test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1200px-Good_Food_Display_-_NCI_Visuals_Online.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
