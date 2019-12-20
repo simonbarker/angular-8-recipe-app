7e3647c61ebd2a3a50697e4b2f2e15e499bbc699
Simon Barker <simonbarker@simon.local>
2019-12-20 21:25:50 +0000

    change from eventEmitters to Subjects

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -14,12 +14,6 @@ export class RecipesComponent implements OnInit {
   constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
-    this.recipesService.recipeSelected
-    .subscribe(
-      (recipe: Recipe) => {
-        this.recipeToDisplay = recipe;
-      }
-    );
   }
 
 }
